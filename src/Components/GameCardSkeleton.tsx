3ea4a6fa3da1ffe16853c8a763fc8d7a2b23119e
jawad ali <jawad.ali.78789000@gmail.com>
2024-09-03 17:00:42 +0500

    genre list

--- src/Components/GameCardSkeleton.tsx
+++ src/Components/GameCardSkeleton.tsx
@@ -1,12 +1,13 @@
-import { Box, Skeleton, SkeletonCircle, SkeletonText } from "@chakra-ui/react";
+import { Box, Skeleton, SkeletonCircle, SkeletonText, useColorModeValue } from "@chakra-ui/react";
 
 export const GameCardSkeleton = () => {
   return (
     <>
-      <Box width={400}  boxShadow="lg" bg="gray.200" borderRadius={10} height={400}>
+      <Box width={400}  boxShadow="lg" bg={useColorModeValue("gray.100", "gray.900")} borderRadius={10} height={400}>
         <SkeletonText overflow={'none'} noOfLines={1} spacing="1" skeletonHeight="225" />
         <SkeletonText mt="5" noOfLines={4} spacing="4" skeletonHeight="2" paddingX={6}/>
       </Box>
     </>
   );
 };
+  
--- src/Components/GenreList.tsx
+++ src/Components/GenreList.tsx
@@ -1,14 +1,45 @@
+import { Box, HStack, Image, Text, useColorModeValue } from "@chakra-ui/react";
 import useData from "../hooks/UseData";
 import useGenre, { Genre } from "../hooks/useGenre";
 export const GenerList = () => {
   const { data, error, isLoading } = useGenre();
   return (
-    <div className="sidebar">
-      {data.map((data) => (
-        <div key={data.id} className="data-item">
-          {data.name}
-        </div>
+    <Box className="sidebar" style={{ padding: "20px", marginTop: "60px" }}>
+      {data.map((item) => (
+        <Box
+          key={item.id}
+          className="data-item"
+          bg={useColorModeValue("white", "gray.700")}
+          padding={2}
+          cursor={"pointer"}
+          marginY={2}
+          borderRadius={8}
+          boxShadow={"0 1px 3px rgba(0, 0, 0, 0.2)"}
+          transition={"transform 0.3s"}
+          onMouseEnter={(e) =>
+            (e.currentTarget.style.transform = "scale(1.05)")
+          }
+          onMouseLeave={(e) => (e.currentTarget.style.transform = "scale(1)")}
+        >
+          <HStack>
+            <Image
+              src={item.image_background}
+              alt={item.name}
+              boxSize="50px"
+              objectFit="cover"
+              mr="15px"
+              borderRadius="10%"
+              boxShadow="0 1px 3px rgba(0, 0, 0, 0.2)"
+            />
+            <Text
+              style={{ fontSize: "16px", fontWeight: "600" }}
+              color={useColorModeValue("gray.700", "white")}
+            >
+              {item.name}
+            </Text>
+          </HStack>
+        </Box>
       ))}
-    </div>
+    </Box>
   );
 };
